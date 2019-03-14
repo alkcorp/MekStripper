1aeb0826f591843c09ce5f156653b6eaed60ee3e
Alkalus <3060479+draknyte1@users.noreply.github.com>
2019-03-14 13:18:37 +0000

    - Reflectively remove all Shaped and Shapeless recipes from Mekanism.

--- src/net/alkalus/mod/mekstrip/CommonProxy.java
+++ src/net/alkalus/mod/mekstrip/CommonProxy.java
@@ -1,10 +1,21 @@
 package net.alkalus.mod.mekstrip;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import mekanism.common.MekanismBlocks;
+import mekanism.common.MekanismItems;
+import mekanism.common.util.RecipeUtils;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class CommonProxy {
 	
@@ -29,13 +40,49 @@ public class CommonProxy {
 	}
 
 	public void onLoadComplete(FMLLoadCompleteEvent event) {
+		//Remove Recipes
+		List<Field> aItems = getFields(MekanismItems.class);
+		List<Field> aBlocks = getFields(MekanismBlocks.class);
+		removeRecipeFromMap(aItems, 0);
+		removeRecipeFromMap(aBlocks, 1);		
 	}
 
+	public void removeRecipeFromMap(List<Field> aData, int type) {
+		for (Field f : aData) {
+			if (f != null) {
+				f.setAccessible(true);
+				Object y;
+				try {
+					y = f.get(null);	
+					if (y != null) {
+						if (y instanceof Item) {							
+							for (short meta=0;meta<Short.MAX_VALUE;meta++) {								
+								if (type == 0) {
+									boolean b = RecipeUtils.removeRecipes(new ItemStack((Item) y, 1, meta));									
+								}
+								else {
+									boolean b = RecipeUtils.removeRecipes(new ItemStack((Block) y, 1, meta));										
+								}															
+							}														
+						}
+					}
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+				}
+			}
+		}
+	}
+	
 	public void registerNetworkStuff() {
 	}
 
-	public void registerRenderThings() {
-
-	}
+	private List<Field> getFields(Class t) {
+        List<Field> fields = new ArrayList<>();
+        Class clazz = t;
+        while (clazz != Object.class) {
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+        return fields;
+    }
 
 }
